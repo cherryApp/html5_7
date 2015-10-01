31b20c2ffebd6649133aae1b062a3f778ff51593
unknown <html@2-00.itfactory.neta>
2015-10-01 18:38:30 +0200

    jQuery ajax teljes példa

--- js/main.js
+++ js/main.js
@@ -20,9 +20,39 @@ function processForm(btn) {
     var serverObj = {
         "user": az,
         "data": data
-    }
+    };
+
+    // Post indítása.
+    $.post("http://37.139.16.100:3000", JSON.stringify(serverObj), function (respons) {
+        console.log(respons);
+    });
 
     console.log(serverObj);
 
     return false;
+}
+
+// Adatok visszakérdezése a szerverről.
+function getData(btn) {
+
+    console.log(btn);
+
+    // Get kérés.
+    $.getJSON("http://37.139.16.100:3000/" + az, function (respons) {
+        console.log(respons);
+
+        // Űrlap kiválasztása.
+        var form = $(".reg-form");
+
+        console.log(form);
+
+        form.find("input").each(function (index, input) {
+            var name = input.name;
+            if (respons[name]) {
+                input.value = respons[name];
+            }
+        });
+
+    });
+
 }
